af4191536eeba4a068ae47b2c54ff9be711dd5dc
yurishe <yurishe00@gmail.com>
2022-11-14 20:50:09 +0100

    Add basic calculator functions

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ const clearButton = document.querySelector(".clear")
 buttons.forEach(button => button.addEventListener('click', () => {
     console.log(button.dataset.value);
 }))
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function substract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
