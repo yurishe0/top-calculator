5d336224dd2ceb787d2341d08704e7e509f02bcd
yurishe <yurishe00@gmail.com>
2022-11-15 21:19:25 +0100

    HTML and CSS adjustments, working screen input and clear function

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 const calculator = document.querySelector(".calculator");
 const screen = document.querySelector(".screen");
+const screenContent = document.querySelector(".screen-content");
 const buttons = document.querySelectorAll(".button");
+const buttonNumbers = document.querySelectorAll(".number");
+const buttonOperators = document.querySelectorAll(".operator");
 const equalsButton = document.querySelector(".equals");
 const clearButton = document.querySelector(".clear")
 
-buttons.forEach(button => button.addEventListener('click', () => {
-    console.log(button.dataset.value);
-}))
+let previousNumber = "";
+let currentNumber = "";
+
+buttonNumbers.forEach(button => button.addEventListener('click', () => {
+    previousNumber = currentNumber;
+    currentNumber += button.dataset.value;
+
+    screenContent.textContent = currentNumber;
+
+    console.log(previousNumber);
+    console.log(currentNumber);
+}));
+
+buttonOperators.forEach(button => button.addEventListener('click', () => {
+    let operator = button.dataset.value;
+    console.log(operator);
+
+    switch(operator) {
+        case "clear":
+            clearScreen();
+            break;
+    }
+}));
+
+function clearScreen() {
+    currentNumber = "";
+    previousNumber = "";
+    screenContent.textContent = "";
+}
 
 function add(num1, num2) {
     return num1 + num2;
