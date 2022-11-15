74196c705f19d395eb9ce19c1bce2efd56ff571b
yurishe <yurishe00@gmail.com>
2022-11-15 21:59:01 +0100

    Add functionality for calculations with only two numbers

--- script.js
+++ script.js
@@ -7,22 +7,45 @@ const buttonOperators = document.querySelectorAll(".operator");
 const equalsButton = document.querySelector(".equals");
 const clearButton = document.querySelector(".clear")
 
-let previousNumber = "";
 let currentNumber = "";
+let currentExpression = [];
 
 buttonNumbers.forEach(button => button.addEventListener('click', () => {
-    previousNumber = currentNumber;
     currentNumber += button.dataset.value;
+    currentNumberDisplay = button.dataset.value;
 
-    screenContent.textContent = currentNumber;
-
-    console.log(previousNumber);
-    console.log(currentNumber);
+    screenContent.textContent += currentNumberDisplay;
 }));
 
 buttonOperators.forEach(button => button.addEventListener('click', () => {
     let operator = button.dataset.value;
-    console.log(operator);
+    let operatorSymbol = "";
+
+    switch(operator) {
+        case "add":
+            operatorSymbol = "+";
+            break;
+        case "substract":
+            operatorSymbol = "-";
+            break;
+        case "multiply":
+            operatorSymbol = "x";
+            break;
+        case "divide":
+            operatorSymbol = "/";
+            break;
+    }
+
+    screenContent.textContent += ` ${operatorSymbol} `;
+    currentExpression.push(currentNumber);
+    currentNumber = "";
+
+    if(currentExpression.length >= 2) {
+        calculate(currentExpression);
+        currentExpression = [];
+        console.log(currentExpression);
+    }
+    currentExpression.push(operator);
 
     switch(operator) {
         case "clear":
@@ -34,21 +57,54 @@ buttonOperators.forEach(button => button.addEventListener('click', () => {
 function clearScreen() {
     currentNumber = "";
     previousNumber = "";
+    currentExpression = [];
     screenContent.textContent = "";
 }
 
 function add(num1, num2) {
-    return num1 + num2;
+    screenContent.textContent = num1 + num2;
 }
 
 function substract(num1, num2) {
-    return num1 - num2;
+    screenContent.textContent = num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    screenContent.textContent = num1 * num2;
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    screenContent.textContent = num1 / num2;
+}
+
+function calculate(expression) {
+    console.log(expression);
+    const num1 = parseInt(expression[0]);
+    const operator = expression[1];
+    const num2 = parseInt(expression[2]);
+
+    console.log(num1, operator, num2);
+
+    switch(operator) {
+        case "add":
+            add(num1, num2);
+            currentExpression = [];
+            currentNumber = [];
+            break;
+        case "substract":
+            substract(num1, num2);
+            currentExpression = [];
+            currentNumber = [];
+            break;
+        case "multiply":
+            multiply(num1, num2);
+            currentExpression = [];
+            currentNumber = [];
+            break;
+        case "divide":
+            divide(num1, num2);
+            currentExpression = [];
+            currentNumber = [];
+            break;
+    }
 }
