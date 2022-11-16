4b1d448e6f87b133099fb62bb93f5edbdb2950ac
yurishe <yurishe00@gmail.com>
2022-11-16 22:07:15 +0100

    Add functionality to calculate all expressions without proper PEDMAS order

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ buttonOperators.forEach(button => button.addEventListener('click', () => {
     let operator = button.dataset.value;
     let operatorSymbol = "";
 
-    switch(operator) {
+    switch (operator) {
         case "add":
             operatorSymbol = "+";
             break;
@@ -40,18 +40,18 @@ buttonOperators.forEach(button => button.addEventListener('click', () => {
     currentExpression.push(currentNumber);
     currentNumber = "";
 
-    if(currentExpression.length >= 2) {
-        calculate(currentExpression);
-        currentExpression = [];
-        console.log(currentExpression);
-    }
+
     currentExpression.push(operator);
 
-    switch(operator) {
+    switch (operator) {
         case "clear":
             clearScreen();
             break;
+        case "equals":
+            calculate(currentExpression);
+            break;
     }
+
 }));
 
 function clearScreen() {
@@ -62,49 +62,61 @@ function clearScreen() {
 }
 
 function add(num1, num2) {
-    screenContent.textContent = num1 + num2;
+    return num1 + num2;
 }
 
 function substract(num1, num2) {
-    screenContent.textContent = num1 - num2;
+    return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    screenContent.textContent = num1 * num2;
+    return num1 * num2;
 }
 
 function divide(num1, num2) {
-    screenContent.textContent = num1 / num2;
+    return num1 / num2;
 }
 
 function calculate(expression) {
+    expression.pop();
+
     console.log(expression);
-    const num1 = parseInt(expression[0]);
-    const operator = expression[1];
-    const num2 = parseInt(expression[2]);
 
-    console.log(num1, operator, num2);
+    for (let i = 0; i <= expression.length; i++) {
 
-    switch(operator) {
-        case "add":
-            add(num1, num2);
-            currentExpression = [];
-            currentNumber = [];
-            break;
-        case "substract":
-            substract(num1, num2);
-            currentExpression = [];
-            currentNumber = [];
-            break;
-        case "multiply":
-            multiply(num1, num2);
-            currentExpression = [];
-            currentNumber = [];
-            break;
-        case "divide":
-            divide(num1, num2);
-            currentExpression = [];
-            currentNumber = [];
-            break;
+        if (i % 2 != 0) {
+
+            i = 1;
+            let currentOperator = expression[i];
+            let number1 = parseInt(expression[i - 1]);
+            let number2 = parseInt(expression[i + 1]);
+
+            console.log(number1, currentOperator, number2);
+            let result = 0;
+
+            switch (currentOperator) {
+                case "add":
+                    result = add(number1, number2);
+                    break;
+                case "substract":
+                    result = substract(number1, number2);
+                    break;
+                case "multiply":
+                    result = multiply(number1, number2);
+                    break;
+                case "divide":
+                    result = divide(number1, number2);
+                    break;
+            }
+
+            expression.splice(i - 1, 3);
+
+            result = result.toString();
+            expression.unshift(result);
+            console.log(expression);
+        }
+
     }
+
+    currentExpression = [];
 }
