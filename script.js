578edd2b4581e8532225b104f78b96a59a981e7f
yurishe <yurishe00@gmail.com>
2022-11-17 16:28:28 +0100

    Add comments

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ const buttonOperators = document.querySelectorAll(".operator");
 
 let currentNumber = "";
 let currentExpression = [];
+
+// Last & previous input are used to prevent putting two operators consecutively.
 let lastInput = "";
 let previousInput = "";
 
+// Listen for clicks on each number button [0 -> 9].
 buttonNumbers.forEach(button => button.addEventListener('click', () => {
     currentNumber += button.dataset.value;
     currentNumberDisplay = button.dataset.value;
+
     previousInput = lastInput;
     lastInput = button.dataset.value;
-
 
     screenContent.textContent += currentNumberDisplay;
 }));
@@ -20,10 +23,11 @@ buttonNumbers.forEach(button => button.addEventListener('click', () => {
 buttonOperators.forEach(button => button.addEventListener('click', () => {
     let operator = button.dataset.value;
     let operatorSymbol = "";
+
     previousInput = lastInput;
     lastInput = button.dataset.value;
 
-
+    // Asign a proper screen display value to each operator on click.
     switch (operator) {
         case "add":
             operatorSymbol = "+";
@@ -58,6 +62,7 @@ buttonOperators.forEach(button => button.addEventListener('click', () => {
 
 }));
 
+// Completely wipes all the existing data.
 function clearScreen() {
     currentNumber = "";
     previousNumber = "";
@@ -65,6 +70,8 @@ function clearScreen() {
     screenContent.textContent = "";
 }
 
+
+// Calculating functions.
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -81,14 +88,18 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+// Main function that calculates the given expression.
+
 function calculate(expression) {
-    expression.pop();
+    expression.pop(); // This pops the "equals" out from the expression array.
 
     for (let i = 0; i <= expression.length; i++) {
 
-        if (i % 2 != 0) {
+        if (i % 2 != 0) { // Look for operators. Operators are always on odd array places.
 
-            i = 1;
+            i = 1; // No clue why, but it breaks without this.
+
+            // Take the operator and the number to the left and right to it.
             let currentOperator = expression[i];
             let number1 = parseFloat(expression[i - 1]);
             let number2 = parseFloat(expression[i + 1]);
@@ -96,6 +107,7 @@ function calculate(expression) {
 
             let result = 0;
 
+            // Check the given operator and use an adequate function.
             switch (currentOperator) {
                 case "add":
                     result = add(number1, number2);
@@ -111,16 +123,17 @@ function calculate(expression) {
                     break;
             }
 
-            expression.splice(i - 1, 3);
+            expression.splice(i - 1, 3); // Remove the calculated elements out from the array.
 
-            result = +result.toFixed(4);
-            expression.unshift(result.toString());
+            result = +result.toFixed(4); // Round to 4 decimal points.
+
+            expression.unshift(result.toString()); // Put the calculated result back to the array.
         }
 
     }
 
-    screenContent.textContent = currentExpression;
+    screenContent.textContent = currentExpression; // After evaluating the entire array, display the result on the user's screen.
 
-    currentNumber = currentExpression[0];
-    currentExpression.shift();
+    currentNumber = currentExpression[0]; // Asign the result as a first element for the next calculation.
+    currentExpression.shift(); // No clue why, but the thing above breaks without this.
 }
